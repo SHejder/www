5b26be77b4bea595d7468b2507f05a56865c6e68
SHejder <syhanov.m@yandex.ru>
2017-08-30 13:06:54 +0300

    add script for button

---
+++ web/assets/scripts/ajax-link.js
@@ -0,0 +1,44 @@
+$(document).ready(function () {
+    $('body')
+        .on('click', 'a[href].ajax', function (e) {
+            e.preventDefault();
+
+            var $link = $(this);
+
+            if ($link.data('clicked')) {
+                return;
+            }
+
+            var linkContent = $link.html();
+            $link.data('clicked', true);
+
+            $.ajax({
+                url: $link.attr('href')
+            }).done(function (html) {
+                $('body').append(
+                    '<div class="window" id="window">' +
+                        '<div class="container" id="window-container">' +
+                            '<a href="#" class="wind_close"><img src="../assets/images/wind_close.png" alt="" title=""/></a>' +
+                            html +
+                        '</div>' +
+                    '</div>'+
+                    '<div class="overlay_dark"></div>'
+                );
+            }).always(function () {
+                $link.removeData('clicked').html(linkContent);
+            }).fail(onAjaxFail);
+        })
+        .on('click', '#window', function (e) {
+            if (e.target === this) {
+                $(this).find('.wind_close:first').trigger('click');
+            }
+        })
+        .on('click', '#window .wind_close', function (e) {
+            e.preventDefault();
+
+            $('#window').remove();
+
+            $('.overlay_dark').remove();
+
+        });
+});
